4aea0b0f679cce83c9d9af4418d3889da33f3464
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2025-08-13 22:17:31 +0530

    Added Core Components of the Application

---
+++ src/components/layouts/Layout.jsx
@@ -0,0 +1,8 @@
+//! This layout includes all the other components bundled into 1 layout, 
+//! so that we could use this layout more often.
+
+export default function Layout(){
+    return (
+        <div></div>
+    )
+}
