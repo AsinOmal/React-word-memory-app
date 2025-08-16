26281697249cd1cbcf2b783abca3b18f213317e1
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2025-08-17 03:12:41 +0530

    Welcome Page HTML Created

--- src/components/layouts/Welcome.jsx
+++ src/components/layouts/Welcome.jsx
@@ -1,5 +1,18 @@
 export default function Welcome() {
     return (
-        <div>Welcome</div>
+        <section id="welcome">
+            <h3 className="text-large special-shadow">365 Days.<br />365 Words.</h3>
+
+            <h6>
+                Build your Lexicon. <br /> Start Your Challenge Today!
+            </h6>
+
+            <div>
+                <input type="text" placeholder="Enter your Name..." />
+                <button>
+                    <h6>Start &rarr;</h6>
+                </button>
+            </div>
+        </section>
     )
 }
