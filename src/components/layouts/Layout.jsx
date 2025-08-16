f38f17292cfbd8e0e2994557901eca56c8e7ccdf
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2025-08-17 03:07:38 +0530

    Added CSS for Layout Component

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -3,15 +3,28 @@
 
 export default function Layout(props) {
 
-    const { children } = props
+    const { children } = props      //This line uses destructuring assignment to extract the children prop from the props object.  
 
     return (
-        <div>
+        <>
+            <header>
+                <h1 className="text-gradient">Copacetic</h1>
+            </header>
 
-            header
-            {children}
-            footer
-            
-        </div>
+            <main>
+                {children}
+            </main>
+
+            <footer>
+                <small>Created by</small>
+                <a target="_blank" href="https://github.com/AsinOmal">
+                    <img src="https://avatars.githubusercontent.com/u/127041794?v=4" alt="pfp" />
+                    <p>@AsinOmal</p>
+                    <i className="fa-brands fa-github"></i>
+                </a>
+                
+            </footer>
+        </>
+
     )
 }
