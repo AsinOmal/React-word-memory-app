d05ec39c4145d56baa8faed344ef6808149c35db
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2025-08-21 00:31:37 +0530

    Created Structure for Countdown Component

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function Countdown() {
     return (
-        <div></div>
+        <div className='card countdown-card'>
+            <h1 className="item-header">Day {1}</h1>
+            <div className="today-container">
+                <div>
+                    <p>Time Remaining</p>
+                    <h3>13H 35M 22S</h3>
+                </div>
+
+                <div>
+                    <p>Words for Today</p>
+                    <h3>16</h3>
+                </div>
+            </div>
+
+            <button className="start-task">
+                <h6>Start</h6>
+            </button>
+        </div>
     )
 }
