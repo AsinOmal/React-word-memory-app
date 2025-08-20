c9232c4a0d6716743cae938cf6771e94884e0221
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2025-08-21 00:24:07 +0530

    Added Styling for Progress Component

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,7 @@
 
 export default function ProgressBar() {
 
-  const text = 'hello world' // TODO: Come and change this later.
+  const text = 'Progress' // TODO: Come and change this later.
 
   const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -13,11 +13,11 @@ export default function ProgressBar() {
 
       {arr.map((element, elementIdx) => {  //element is the current item in the array, //elementIdx is the index of that item.
         return (
-          <div className="level-bar" key={elementIdx}>{element}</div>
+          <div className="level-bar" key={elementIdx}>{}</div>
         )
       })}
 
-      <div className="xp" style={{ width: `${40}%` }}></div>
+      <div className="xp" style={{ width: `${8}%` }}></div>
 
     </div>
   )
